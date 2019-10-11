d5eff6aaf7391a1a558a398fc5d5e420a1412493
24395 <32060861+24395@users.noreply.github.com>
2019-10-12 08:56:59 +1100

    Update GoalData.h

--- Morph/lib/Data Structs/GoalData.h
+++ Morph/lib/Data Structs/GoalData.h
@@ -7,7 +7,8 @@
 class GoalData{
 
     public: 
-        uint16_t angle, distance;
+        uint16_t angle 
+        uint8_t distance;
         bool face;
 
         GoalData(){
@@ -15,7 +16,7 @@ class GoalData{
             distance = 0;
         }
 
-        GoalData(uint16_t angle, uint16_t distance) : angle(angle), distance(distance) {}
+        GoalData(uint16_t angle, uint8_t distance) : angle(angle), distance(distance) {}
 
         bool visible(){
             return angle != 65535;
